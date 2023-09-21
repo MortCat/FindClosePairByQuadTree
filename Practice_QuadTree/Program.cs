5a39e4767d953100ef66216b8c71007249f5641c
MortCat <mortcj0@gmail.com>
2023-09-21 23:30:54 +0800

    加入專案檔案。

---
+++ Practice_QuadTree/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace TrafficControlTest
+{
+	static class Program
+	{
+		static void Main()
+		{
+			List<Point> positions = new List<Point>
+			{
+				new Point(22,21),
+				new Point(23,26),
+				new Point(100,20),
+				new Point(20,100),
+				new Point(85,66),
+				new Point(1,3),
+				new Point(15,22)
+			};
+
+			int minX = 0, minY = 0, maxX = 100, maxY = 100;
+			QuadTree quadTree = new QuadTree(new Rect(minX, minY, maxX, maxY)); // 請根據你的實際情況提供邊界範圍
+			foreach (var point in positions)
+			{
+				quadTree.Insert(point);
+			}
+
+			List<(Point, Point)> closePair = FindClosePairs(quadTree);
+			foreach (var branch in closePair)
+			{
+				Console.WriteLine($"Point1: {branch.Item1.X},{branch.Item1.Y} and Point2: {branch.Item2.X},{branch.Item2.Y} ");
+			}
+			Console.ReadKey();
+
+
+
+		}
+		private static List<(Point, Point)> FindClosePairs(QuadTree quadTree)
+		{
+			List<(Point, Point)> closePair = new List<(Point, Point)>();
+			if (quadTree.IsLeafNode())
+			{
+				for (int i = 0; i < quadTree.Points.Count; i++)
+				{
+					for (int j = 0; j < quadTree.Points.Count; j++)
+					{
+						double distance = CalculateDistance(quadTree.Points[i], quadTree.Points[j]);
+						if (distance < 10)
+						{
+							closePair.Add((quadTree.Points[i], quadTree.Points[j]));
+						}
+
+					}
+				}
+			}
+
+			return closePair;
+		}
+		private static int CalculateDistance(Point p1, Point p2)
+		{
+			return (int)Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+		}
+
+	}
+}
---
+++ Practice_QuadTree/QuadTree.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+public class QuadTree
+{
+    public Rect Boundary { get; private set; }
+    public List<Point> Points { get; private set; }
+    public QuadTree[] Children { get; private set; }
+
+    public QuadTree(Rect boundary)
+    {
+        Boundary = boundary;
+        Points = new List<Point>();
+        Children = null;
+    }
+
+    // 將點插入QuadTree
+    public void Insert(Point point)
+    {
+        if (!Boundary.Contains(point))
+            return;
+
+        if (IsLeafNode())
+        {
+            Points.Add(point);
+            // 範圍容許數量的閥值，超過就分割
+            if (Points.Count > 5)
+                Subdivide();
+        }
+        else
+        {
+            foreach (var child in Children)
+            {
+                child.Insert(point);
+            }
+        }
+    }
+
+    public bool IsLeafNode()
+    {
+        return Children == null;
+    }
+
+    public void Subdivide()
+    {
+        double xMid = (Boundary.Left + Boundary.Right) / 2;
+        double yMid = (Boundary.Top + Boundary.Bottom) / 2;
+
+        Children = new QuadTree[4];
+        Children[0] = new QuadTree(new Rect(xMid, yMid, Boundary.Right, Boundary.Top));
+        Children[1] = new QuadTree(new Rect(Boundary.Left, yMid, xMid, Boundary.Top));
+        Children[2] = new QuadTree(new Rect(Boundary.Left, Boundary.Bottom, xMid, yMid));
+        Children[3] = new QuadTree(new Rect(xMid, Boundary.Bottom, Boundary.Right, yMid));
+    }
+}
